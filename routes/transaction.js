017d67f8edc15538fdaef31ea73c2a2767f10f23
Farhad Yousefe <farhadyousefe@gmail.com>
2026-07-03 19:59:52 +0430

    add transaction route

---
+++ routes/transaction.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import Debug from 'debug';
+import { transaction, validateTransaction } from './models/transaction.js';
+
+const dbDebug = Debug('app:db');
+dbDebug.color = 2;
+const httpDebug = Debug('app:express');
+httpDebug.color = 3;
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  httpDebug('A post request is made');
+  const { error, value } = validateTransaction(req.body);
+
+  if (error) {
+    httpDebug(`Joi Validation Error: ${error.details[0].message}`);
+    return res
+      .status(400)
+      .send(`Joi Validation Error: ${error.details[0].message}`);
+  }
+  try {
+    const newTransaction = new transaction(value);
+    await newTransaction.save();
+    return res.status(201).json(newTransaction);
+  } catch (error) {
+    httpDebug(`Error in server: ${error.message}`);
+    return res.status(500).send(`Error in server`);
+  }
+});
+
+export default router;
