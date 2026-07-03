c5b9cc9c3b3fd3c1df58d2fb0be70866126055b6
Farhad Yousefe <farhadyousefe@gmail.com>
2026-07-03 21:09:15 +0430

    build the index.js from scratch

---
+++ index.js
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import Debug from 'debug';
+import TransactionRouter from './routes/transaction.js';
+
+const dbDebug = Debug('app:db');
+dbDebug.color = 2;
+const httpDebug = Debug('app:express');
+httpDebug.color = 3;
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const database = process.env.DATABASE_URL;
+
+mongoose
+  .connect(database)
+  .then(() => dbDebug('Database connected successfully'))
+  .catch((err) => {
+    dbDebug(`error in connection: ${err.message}`);
+    process.exit(1);
+  });
+
+app.use(express.json());
+app.use('/api/transactions', TransactionRouter);
+
+app.listen(PORT, () => {
+  httpDebug(`Listening on PORT: ${PORT}`);
+});
