26644c95a780b5d5ae2d768cc283945d72e445c3
Farhad Yousefe <farhadyousefe@gmail.com>
2026-07-05 15:06:43 +0430

    import authRouter, add privateKey and a check for it, use authRouter for /api/v1/auth request

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
 import express from 'express';
 import mongoose from 'mongoose';
 import Debug from 'debug';
 import TransactionRouter from './routes/transaction.js';
 import errorHandler from './middleware/error.js';
+import authRouter from './routes/auth.js';
 
 const dbDebug = Debug('app:db');
 dbDebug.color = 2;
@@ -14,6 +16,11 @@ httpDebug.color = 3;
 const app = express();
 const PORT = process.env.PORT || 3000;
 const database = process.env.DATABASE_URL;
+const privateKey = process.env.JWT_PRIVATE_KEY;
+if (!privateKey) {
+  httpDebug('FATAL ERROR: JWT_PRIVATE_KEY is not defined in the environment.');
+  process.exit(1);
+}
 
 mongoose
   .connect(database)
@@ -24,7 +31,8 @@ mongoose
   });
 
 app.use(express.json());
-app.use('/api/transactions', TransactionRouter);
+app.use('/api/v1/transactions', TransactionRouter);
+app.use('/api/v1/auth', authRouter);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
