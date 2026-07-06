bd5a8f7ba749ac2ca7209b4b62e50a0d0f652096
Farhad Yousefe <farhadyousefe@gmail.com>
2026-07-06 16:42:01 +0430

    fix typos in Debug app:startup

--- middleware/error.js
+++ middleware/error.js
@@ -1,11 +1,11 @@
 import Debug from 'debug';
 
-const appDebug = Debug('sppz:error');
+const appDebug = Debug('app:startup');
 
 function errorHandler(err, req, res, next) {
   appDebug(`Global Exception Error: ${err.stack}`);
 
-  res.status(500).send(`Something went wrong on the server`);
+  res.status(500).send(`Something went wrong on the server side`);
 }
 
 export default errorHandler;
